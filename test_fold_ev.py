42e235406b93701fa0a18c7d495368a81ad42900
Arif <arif@themascoteers.com>
2023-09-21 22:44:45 +1000

    done

--- test_fold_ev.py
+++ test_fold_ev.py
@@ -1,5 +1,5 @@
 from fold_evaluator import FoldEvaluator
 
 if __name__ == "__main__":
-    c = FoldEvaluator(prefix="no_es2", folds=10, algorithms=["mlr","svr","ann"])
+    c = FoldEvaluator(prefix="no_es2", folds=10, algorithms=["mlr","svr","ann","ann_es"])
     c.process()
